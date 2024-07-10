68775f1375f82d30411313d3025db2484ffdae96
EventfulLynx915 <gavrylenko.anton@gmail.com>
2024-07-10 15:35:54 +0200

    Create svg icon component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-import assets from "astro/dist/assets/vite-plugin-assets.js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +8,6 @@ export default defineConfig({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: false
-  }), icon()],
-  experimental: { assets: true },
+  })],
 
 });
