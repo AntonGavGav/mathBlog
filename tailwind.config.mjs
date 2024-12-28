33a5b8db77164ba37a1465e28b522b452ae860a1
EventfulLynx915 <57542365+AntonGavGav@users.noreply.github.com>
2024-12-28 18:24:58 +0100

    Responsiveness to a website (#5)
    * Start implementing responsivness
    
    * Add working nav-button to header
    
    * Responsiveness for main page(if it works, it works)
    
    * Start working on responsiveness for about page
    
    * Finish responsiveness on about page
    
    * Small tweaks on blog page
    
    * Responsive search bar
    
    * SearchBar improve and responsiveness changes
    
    * fix of issue with search accessability
    
    * finish responsiveness on blog page
    
    * improve styling for blog post
    
    * finish responsiveness on blog post pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ module.exports = {
 	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens:{
+			// keep in mind that if you want to change the sizes go to showBlurBg in SearchWidget.astro and change the media there as well
+			'xl': {'max': '1130px'},
+			'lg': {'max': '740px'},
+			'md': {'max': '470px'}
+		},
 		fontFamily:{
 			'sans': ['Videotype', 'sans-serif'],
 			'inter': ['Inter'],
@@ -21,6 +27,9 @@ module.exports = {
 				'light-accent': 'var(--light-accent)',
 			}
 		},
+	},
+	corePlugins: {
+		container: false,
 	},
 	plugins: [],
 }
